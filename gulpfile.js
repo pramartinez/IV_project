022c9aef41c3cb5d8d39258f12ecaea7f6b31386
pramartinez <pramtnez@gmail.com>
2019-10-28 00:22:38 +0100

    Usando gulp para iniciar pm2 closes #44 closes #47

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+var jest = require('gulp-jest').default;
+const pm2 = require('pm2');
+const shell = require('gulp-shell');
+
+
+gulp.task('test', () => (
+    gulp.src('test', {read: false})
+        .pipe(jest())
+));
+
+gulp.task('start', function () {
+  pm2.connect(true, function () {
+    pm2.start({
+      name: 'VPTournaments',
+      script: 'bin/www',
+      exec_mode: 'cluster',
+      instances: 4
+    }, function () {
+         console.log('Arrancando VPTournaments.');
+         pm2.streamLogs('all', 0);
+       });
+  });
+});
+
+gulp.task('stop', shell.task(['pm2 stop VPTournaments' ]));
