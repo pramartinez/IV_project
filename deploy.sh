6a0d5c06165ad4ee53d89be9ec3d085f96fbec61
pramartinez <pramtnez@gmail.com>
2019-11-15 16:24:29 +0100

    Modificando deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
-echo "-----------------Variables----------------------"
+echo "-----------------Variables---------------------------------"
 echo "DEPLOYMENT_SOURCE = $DEPLOYMENT_SOURCE"
 echo "DEPLOYMENT_TARGET = $DEPLOYMENT_TARGET"
 echo "NEXT_MANIFEST_PATH = $NEXT_MANIFEST_PATH"
 echo "PREVIOUS_MANIFEST_PATH = $PREVIOUS_MANIFEST_PATH"
+echo "KUDU_SYNC_CMD = $appdata$\npm\kuduSync.cmd"
+echo "-----------------Variables END ---------------------------------"
 echo ""
 echo ""
+
 
 echo =======  Executing gulp task release  ======= 
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
