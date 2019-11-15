36cb42fd84a9112981f63de7e175a224972be794
pramartinez <pramtnez@gmail.com>
2019-11-15 16:20:17 +0100

    Modificando deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 echo "-----------------Variables---------------------------------"
-echo "DEPLOYMENT_SOURCE = %DEPLOYMENT_SOURCE%"
-echo "DEPLOYMENT_TARGET = %DEPLOYMENT_TARGET%"
-echo "NEXT_MANIFEST_PATH = %NEXT_MANIFEST_PATH%"
-echo "PREVIOUS_MANIFEST_PATH = %PREVIOUS_MANIFEST_PATH%"
-echo "KUDU_SYNC_CMD = %appdata%\npm\kuduSync.cmd"
+echo "DEPLOYMENT_SOURCE = $DEPLOYMENT_SOURCE"
+echo "DEPLOYMENT_TARGET = $DEPLOYMENT_TARGET"
+echo "NEXT_MANIFEST_PATH = $NEXT_MANIFEST_PATH"
+echo "PREVIOUS_MANIFEST_PATH = $PREVIOUS_MANIFEST_PATH"
+echo "KUDU_SYNC_CMD = $appdata$\npm\kuduSync.cmd"
 echo "-----------------Variables END ---------------------------------"
 echo ""
 echo ""
 
 
-echo =======  Executing gulp task release: Starting at %TIME% ======= 
+echo =======  Executing gulp task release  ======= 
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
-    echo "Running gulp start"
+    echo "Running gulp start &"
     cd "$DEPLOYMENT_SOURCE"
     eval './node_modules/.bin/gulp build'
     IF !ERRORLEVEL! NEQ 0 goto error
     popd
 fi
-echo =======  Executing Gulp task release: Finished at %TIME% =======
